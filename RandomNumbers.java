a2eb675c6c9fffcd1c6bdff38dc498b4d66b9905
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-03-01 00:04:55 -0500

    Add random number generating java file

---
+++ RandomNumbers.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+class RandomNumbers {
+
+    private static int[] randomNumbers(int n) {
+        int numbers[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int i = 0;
+        while (i < n) {
+            int r = ThreadLocalRandom.current().nextInt(10);
+            numbers[r] += 1;
+            i += 1;
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        int r[];
+        if (args.length == 0) {
+            r = randomNumbers(10);
+        } else {
+            r = randomNumbers(Integer.parseInt(args[0]));
+        }
+        System.out.print("0: " + r[0] + "\n1: " + r[1] + "\n2: " + r[2] + "\n3: " + r[3] + "\n4: " + r[4] + "\n5: "
+                + r[5] + "\n6: " + r[6] + "\n7: " + r[7] + "\n8: " + r[8] + "\n9: " + r[9] + "\n");
+    }
+}
--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo JAVA
+javac RandomNumbers.java
+java RandomNumbers $1
+rm RandomNumbers.class
+echo
 echo PYTHON
 python random_numbers.py $1
 echo
